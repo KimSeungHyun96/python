aa3a1dda369f7a363b9f6f6ac8da009f0d242eab
SeungHyun7272 <SeungHyun7272@gmail.com>
2021-04-26 18:00:52 +0900

    셀레니움_메일자동화

---
+++ pyexe/seleniumProject/pyexe/seleniumProcess_ksh.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 라이브러리 선언
+import pandas as pd
+from selenium import webdriver
+
+# 웹브라우저 설정
+options = webdriver.ChromeOptions()
+driverLoc = "../addon/chromedriver_win32/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=driverLoc, options=options)
+options.add_argument("window-size=1920x1080")
+
+# 브라우저 열기
+targetUrl = "http://www.coor.kr/"
+driver.get(targetUrl)
+
+# 브라우저 설정 (카테고리 store)
+storeBtnXpath = '//*[@id="aside"]/ul/li[2]/a' 
+quizBtn = driver.find_element_by_xpath(storeBtnXpath).click()
+
+# 브라우저 설정 (카테고리 T-shirt)
+tshirtBtnXpath = '//*[@id="aside"]/ul/li[8]/a' 
+quizBtn = driver.find_element_by_xpath(tshirtBtnXpath).click()
+
+# 라이브러리 선언
+import requests, bs4
+# 페이지 URL Source 가져오기
+driver.current_url
+driver.page_source
+
+url = driver.current_url
+
+# 주소 가져오기 (resp = driver.current_url)
+resp = requests.get(url)
+# encoding = utf-8 / ms949
+resp.encoding='ms949'
+html = resp.text
+# parser를 이용해 정리
+bs = bs4.BeautifulSoup(html, 'html.parser')
+
+itemTag = bs.find("div", {"class":"listItem"})
+item = itemTag.find_all("p")
+price = itemTag.find_all("li", {"class":"price3"})
+
+nameList = []
+priceList = []
+
+nameList = item[0].text
+priceLsit = price[0].text
+
+rowList = []
+columnList = []
+
+for i in range(0,len(item)):
+    nameList = item[i].text.replace("\n", "")
+    priceLsit = price[i].text
+    columnList = [nameList, priceLsit]
+    rowList.append(columnList)
+columnList = []
+
+product = pd.DataFrame(rowList, columns=["상품", "가격(원)"])
+# csv파일로 저장
+product.to_csv("./selenium_KSH.csv", index=False)
+
+# bat 파일 만들기
+bat = open("seleniumProcess_ksh.bat", "w", encoding="utf-8")
+bat.write("python seleniumProcess_ksh.py\npause")
+bat.close()
+
+import smtplib
+from email.message import EmailMessage
+
+# GMAIL 메일 설정
+smtp_gmail = smtplib.SMTP('smtp.gmail.com', 587)
+# 서버 연결을 설정하는 단계
+smtp_gmail.ehlo()
+# 연결을 암호화
+smtp_gmail.starttls()
+#로그인
+userid = "seunghyun7272"
+
+import pickle  # 패스워드 암호화
+
+# 제출용 (비밀번호는 save로 암호화한 후 load만 사용)
+mypass = 'dvckftvywppbltfu'
+
+# save
+with open('ksh.pickle', 'wb') as f:
+   pickle.dump(mypass, f, pickle.HIGHEST_PROTOCOL)
+
+# load
+with open('ksh.pickle', 'rb') as f:
+    pwpickle = pickle.load(f)
+
+smtp_gmail.login(userid, pwpickle)
+
+# import pandas as pd
+# 저장된 csv 파일 불러오기
+# emaillist = pd.read_csv("./email.txt")
+# 디버깅용
+# emaillist
+
+# 이메일 주소정보 리스트 변환
+# to = emaillist.이메일.tolist()
+# 디버깅용
+# to
+# to출력시 나오는 이메일 값을 복사
+# to = ["seunghyun7272@gmail.com", "zmxnc10@naver.com"]
+
+msg=EmailMessage()
+# 제목 입력
+msg['Subject']="중간시험_김승현"
+# 내용 입력
+msg.set_content("셀레니움_자동화_4기_김승현")
+# 보내는 사람
+msg['From'] = 'seunghyun7272@gmail.com'
+# 받는 사람
+msg['To'] = 'seunghyun7272@gmail.com'
+
+# 첨부파일 #1 추가
+file = "selenium_KSH.csv"
+
+fp = open(file,'rb')
+file_data=fp.read()
+
+msg.add_attachment(file_data,
+                    maintype='text',
+                    subtype='plain',
+                    filename=file)
+
+smtp_gmail.send_message(msg)
+smtp_gmail.close()
+
